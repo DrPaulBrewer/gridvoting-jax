4af8df0dc997098af0b22a920f1d4906bcc8e743
Paul Brewer <drpaulbrewer@gmail.com>
2026-01-02 16:33:01 -0500

    remove legacy code for lazy chains

--- src/gridvoting_jax/dynamics/lazy/__init__.py
+++ src/gridvoting_jax/dynamics/lazy/__init__.py
@@ -1,11 +1,9 @@
 """Lazy matrix construction for memory-efficient large-scale models."""
     
 from .utils import should_use_lazy, estimate_memory_for_dense_matrix
-from .lazy_markov import FlexMarkovChain
 
 __all__ = [
     'should_use_lazy', 
     'estimate_memory_for_dense_matrix',
-    'FlexMarkovChain'
 ]
 
